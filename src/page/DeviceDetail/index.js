dc1932842b99c47cf6567c6799d48966a5f7709f
Aditya Kurnia Rahman <aditya.krohman@gmail.com>
2023-03-18 21:15:17 +0700

    fix cover in detail device not full

--- src/page/DeviceDetail/index.js
+++ src/page/DeviceDetail/index.js
@@ -11,37 +11,37 @@ const DeviceDetailPage = props => {
     <View style={pageStyle.Container}>
       <HeaderBar text="Device Detail" type="device-detail" />
       <StatusBar backgroundColor="#6C92F4" />
+      {/* device detail container start*/}
+      <View style={pageStyle.DeviceDetailContainer}>
+        {/* device qr code container start*/}
+        <View style={pageStyle.DeviceQrCodeContainer}>
+          <Image
+            source={{
+              uri: 'https://firebasestorage.googleapis.com/v0/b/device-service-1029d.appspot.com/o/qr.iot.4efd74d2-9d6e-4a59-9aa7-e49b81e25536.PAIRING_USER_TO_DEVICE.png?alt=media&token=qr.iot.4efd74d2-9d6e-4a59-9aa7-e49b81e25536.PAIRING_USER_TO_DEVICE.png',
+            }}
+            style={{
+              width: 120,
+              height: 120,
+            }}
+          />
+        </View>
+        {/* device qr code container stop*/}
+        {/* device spec container start*/}
+        <View style={pageStyle.DeviceSpecContainer}>
+          <Text style={pageStyle.TextDeviceId}>ID</Text>
+          <Text style={pageStyle.TextDeviceIdValue}>
+            iot.3f65ac60-f03c-40ab-bc0a- 1bbbe965f68b
+          </Text>
+          <Text style={pageStyle.TextSensor}>Sensor</Text>
+          <Text style={pageStyle.TextSensorValue}>4</Text>
+          <Text style={pageStyle.TextSensor}>System Uptime</Text>
+          <Text style={pageStyle.TextSensorValue}>100000</Text>
+        </View>
+        {/* device spec container stop*/}
+      </View>
+      {/* device detail container stop*/}
       {/*  content container*/}
       <View style={pageStyle.ContentContainer}>
-        {/* device detail container start*/}
-        <View style={pageStyle.DeviceDetailContainer}>
-          {/* device qr code container start*/}
-          <View style={pageStyle.DeviceQrCodeContainer}>
-            <Image
-              source={{
-                uri: 'https://firebasestorage.googleapis.com/v0/b/device-service-1029d.appspot.com/o/qr.iot.4efd74d2-9d6e-4a59-9aa7-e49b81e25536.PAIRING_USER_TO_DEVICE.png?alt=media&token=qr.iot.4efd74d2-9d6e-4a59-9aa7-e49b81e25536.PAIRING_USER_TO_DEVICE.png',
-              }}
-              style={{
-                width: 120,
-                height: 120,
-              }}
-            />
-          </View>
-          {/* device qr code container stop*/}
-          {/* device spec container start*/}
-          <View style={pageStyle.DeviceSpecContainer}>
-            <Text style={pageStyle.TextDeviceId}>ID</Text>
-            <Text style={pageStyle.TextDeviceIdValue}>
-              iot.3f65ac60-f03c-40ab-bc0a- 1bbbe965f68b
-            </Text>
-            <Text style={pageStyle.TextSensor}>Sensor</Text>
-            <Text style={pageStyle.TextSensorValue}>4</Text>
-            <Text style={pageStyle.TextSensor}>System Uptime</Text>
-            <Text style={pageStyle.TextSensorValue}>100000</Text>
-          </View>
-          {/* device spec container stop*/}
-        </View>
-        {/* device detail container stop*/}
         {/* device Attached Sensor*/}
         <View style={pageStyle.AttachedSensorContainer}>
           <View style={pageStyle.AttachedSensorHeader}>
